ba4ab5415e9164993e719c36cd704a6d98de8468
Andrew Smith <andrewsmith@Andrews-iMac.local>
2021-08-25 20:05:19 -0300

    Almost beaten level 5.

--- player.rb
+++ player.rb
@@ -9,6 +9,7 @@ class Player
     @occupied_spaces = warrior.listen
     @captive_spaces = []
     @enemy_spaces = []
+    @bound_enemy_directions ||= []
     @directions = [:forward, :right, :backward, :left]
 
     @occupied_spaces.each do |occupied_space|
@@ -16,36 +17,58 @@ class Player
       @enemy_spaces << occupied_space if occupied_space.enemy?
     end
 
-    occupied_directions = feel_around
+    @occupied_directions = feel_around_for_units
+    feel_around_for_stairs_and_walls
+    feel_around_for_empty_space
+    # @stairs_direction = warrior.feel.stairs
     pp "@captive_directions"
     pp @captive_directions
     pp "@enemy_directions"
     pp @enemy_directions
-    pp "occupied_directions"
-    pp occupied_directions
+    pp "@occupied_directions"
+    pp @occupied_directions
 
     warrior_action
   end
 
   def warrior_action
+    @enemy_direction = @enemy_directions.first
+    @bound_enemy_direction = @bound_enemy_directions.first
+    @captive_direction = @captive_directions.first
+    stairs_direction = @warrior.direction_of_stairs
+
     if captives_present_and_no_adjacent_enemies?
-      captive_direction = @warrior.direction_of(@captive_spaces.first)
-      walk_to(captive_direction)
+      walk_to_captives
     elsif multiple_adjacent_unbound_enemies?
-      bind_enemy(@enemy_directions.first)
+      bind_enemy(@enemy_direction)
     elsif one_adjacent_unbound_enemy?
-      attack_enemy(@captive_directions.first)
+      attack_enemy(@enemy_direction)
     elsif adjacent_captive?
-      rescue_captive(@captive_directions.first)
+      captive_is_an_enemy ? attack_enemy(@bound_enemy_direction) : rescue_captive(@captive_direction)
     # All captives rescued and no enemies in path.
     else
-      stairs_direction = @warrior.direction_of_stairs
       walk_to(stairs_direction)
+    end
+  end
+
+  def walk_to_captives
+    captive_direction = @warrior.direction_of(@captive_spaces.first)
+
+    if captive_direction != @stairs_direction
+      walk_to(captive_direction)
+    else
+      @empty_directions.each do |empty_direction|
+        empty_direction == @stairs_direction ? next : walk_to(empty_direction)
+      end
     end
   end
 
   def captives_present_and_no_adjacent_enemies?
     @captive_spaces.any? && @captive_directions.empty? && @enemy_directions.empty?
+  end
+
+  def stairs_not_in_path?
+    captive_direction != @stairs_direction.first
   end
 
   def multiple_adjacent_unbound_enemies?
@@ -60,7 +83,20 @@ class Player
     @captive_directions.any?
   end
 
-  def feel_around
+  def captive_is_an_enemy
+    @bound_enemy_direction == @captive_direction
+  end
+
+  def feel_around_for_stairs_and_walls
+    @stairs_direction = feel_for("stairs").first
+    @wall_directions = feel_for("wall")
+  end
+
+  def feel_around_for_empty_space
+    @empty_directions = feel_for("empty")
+  end
+
+  def feel_around_for_units
     @captive_directions = feel_for("captive")
     @enemy_directions = feel_for("enemy")
   end
@@ -70,11 +106,18 @@ class Player
 
     @directions.each do |direction|
       space = @warrior.feel(direction)
-      enemy_present = target == "enemy" && space.enemy?
-      captive_present = target == "captive" && space.captive?
 
-      if enemy_present || captive_present
-        target_directions << direction
+      case target
+      when "enemy"
+        target_directions << direction if space.enemy?
+      when "captive"
+        target_directions << direction if space.captive? && direction != @bound_enemy_directions
+      when "stairs"
+        target_directions << direction if space.stairs?
+      when "wall"
+        target_directions << direction if space.wall?
+      when "empty"
+        target_directions << direction if space.empty?
       end
     end
 
@@ -82,12 +125,13 @@ class Player
   end
 
   def bind_enemy(direction)
+    @bound_enemy_directions << direction
     @warrior.bind!(direction)
     return true
   end
 
   def attack_enemy(direction)
-    @warrior.attack!(@enemy_directions.first)
+    @warrior.attack!(direction)
     return true
   end
 
@@ -97,15 +141,26 @@ class Player
   end
 
   def walk_to(direction)
+    @bound_enemy_directions = []
     @warrior.walk!(direction)
     return true
   end
 
 end
 
+# class PlayerSurroundings
+#   attr_accessor :bound_enemy_directions, :captive_directions, :enemy_directions, :stairs_direction, :wall_directions
 
-#  ----
-# |C s |
-# | @ S|
-# |C s>|
-#  ----
+#   def initialize(options)
+#     @captive_directions = options[:captive_directions]
+#     @enemy_directions = options[:enemy_directions]
+#     @stairs_direction = options[:stairs_direction]
+#     @wall_directions = options[:wall_directions]
+#     @empty_directions = options[:empty_directions]
+#   end
+# end
+
+#  -----
+# |    S|
+# |@> SC|
+#  -----
